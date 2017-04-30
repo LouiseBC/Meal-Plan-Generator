0b2366e637160d3d1ae622b4f13aae56c6ee3b50
Louise Bicker Caarten <louisehmbc@gmail.com>
2017-04-29 22:14:41 -0400

    Show Tolerable Upper Limit For Nutrients

--- scripts/requirements.js
+++ scripts/requirements.js
@@ -10,16 +10,18 @@ if (Meteor.isClient) {
 			if (gender && weight && height && age) {
 				var user = new User(gender, height, weight, age, activity);
 				Session.set('reqsMin', user.reqsMin);
+				Session.set('reqsMax', user.reqsMax);
 			}
 		}
 	});
 
 	Template.nutrientReqs.helpers({
-		'getReqsMin': function() {
-			var reqs = Session.get('reqsMin');
+		'getReqs': function() {
+			var min = Session.get('reqsMin');
+			var max = Session.get('reqsMax');
 			var units = db_units.findOne();
-			reqs = _.map(reqs, function(val,key){ return {nutrient: key, value: val, unit:units[key]} });
-			return reqs.slice(1);
+			min = _.map(min, function(val,key){ return {nutrient: key, value: val, maxVal:(max[key]>="0"?max[key]:"-"), unit:units[key]} });
+			return min.slice(1);
 		}
 	});
 }
